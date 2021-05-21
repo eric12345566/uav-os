7c2544e3d3de33e01f27ab3503a9ebfd0dea3386
Eric Shih <eric12345566@gmail.com>
2021-05-21 16:51:19 +0800

    Backed Up FrameProcess

--- uav-os/process/FrameProcess.py
+++ uav-os/process/FrameProcess.py
@@ -9,63 +9,71 @@ def FrameProcess(shareFrame, OSStateService):
     # cap = cv.VideoCapture(0)
 
     # OSStateService.frameInitReady()
-    if OSStateService.getMode() != 'test':
-        initCapOneTime = False
-        while True:
-            # print("ctr: ",OSStateService.getControllerInitState())
-            if OSStateService.getControllerInitState():
+    initCapOneTime = False
+    while True:
+        # print("ctr: ",OSStateService.getControllerInitState())
+        if OSStateService.getControllerInitState():
 
-                if not initCapOneTime:
-                    address = shareFrame.getAddress()
-                    cap = cv.VideoCapture(address)
-                    initCapOneTime = True
+            if not initCapOneTime:
+                address = shareFrame.getAddress()
+                cap = cv.VideoCapture(address)
+                initCapOneTime = True
 
-                if not cap.isOpened():
-                    logger.fp_error("VideoCapture not opened")
-                    exit(-1)
+            if not cap.isOpened():
+                logger.fp_error("VideoCapture not opened")
+                exit(-1)
 
-                ret, frame = cap.read()
-                shareFrame.setFrame(frame)
+            ret, frame = cap.read()
+            frame = cv.flip(frame, 1)
+            shareFrame.setFrame(frame)
 
-                if not ret:
-                    logger.fp_error("frame empty")
-                    break
+            if not ret:
+                logger.fp_error("frame empty")
+                break
+            shareFrame.frame_frameReady()
 
-                cv.imshow('frame', frame)
-                if cv.waitKey(1) & 0xFF == ord('q'):
-                    break
-                OSStateService.frameInitReady()
-        cap.release()
-        cv.destroyAllWindows()
-        logger.fp_debug("End")
-    else:
-        """ 用本機電腦當測試
-        """
-        logger.fp_debug("In Testing mode, Not open Frame")
-        cap = cv.VideoCapture(0)
-        OSStateService.frameInitReady()
-        while True:
-            # print("ctr: ",OSStateService.getControllerInitState())
-            if OSStateService.getControllerInitState():
-
-                if not cap.isOpened():
-                    logger.fp_error("VideoCapture not opened")
-                    exit(-1)
-
-                ret, frame = cap.read()
-                shareFrame.setFrame(frame)
-
-                if not ret:
-                    logger.fp_error("frame empty")
-                    break
-
-                cv.imshow('frame', frame)
-                if cv.waitKey(1) & 0xFF == ord('q'):
-                    break
-                OSStateService.frameInitReady()
-        cap.release()
-        cv.destroyAllWindows()
-        logger.fp_debug("End")
+            cv.imshow('frame', frame)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+            OSStateService.frameInitReady()
+    cap.release()
+    cv.destroyAllWindows()
+    logger.fp_debug("End")
 
 
+def FrameProcessTest(shareFrame, OSStateService):
+    logger = LoggerService()
+    logger.fp_debug("Start")
+    """ 用本機電腦當測試
+           """
+    logger.fp_debug("In Testing mode, Not open Frame")
+    cap = cv.VideoCapture(0)
+    OSStateService.frameInitReady()
+    while True:
+        # print("ctr: ",OSStateService.getControllerInitState())
+        if OSStateService.getControllerInitState():
 
+            if not cap.isOpened():
+                logger.fp_error("VideoCapture not opened")
+                exit(-1)
+
+            ret, frame = cap.read()
+            shareFrame.setFrame(frame)
+
+            if not ret:
+                logger.fp_error("frame empty")
+                break
+
+            shareFrame.frame_frameReady()
+            # while not markedFrameService.isMarkedFrameReady():
+            #     pass
+            #
+            # markedFrame = markedFrameService.getFrame()
+
+            cv.imshow('frame', frame)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+            OSStateService.frameInitReady()
+    cap.release()
+    cv.destroyAllWindows()
+    logger.fp_debug("End")
